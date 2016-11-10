9bc453a5e01efff334ed21a66bafed595f3a43df
xonmai <xon.mai@gmail.com>
2016-11-10 14:33:23 +0700

    update OrderingService

--- src/service/OrderingService.java
+++ src/service/OrderingService.java
@@ -1,8 +1,12 @@
 package service;
 
+import java.util.List;
+
 import online.food.ordering.Ordering;
 
 public interface OrderingService {
 
 	public Integer saveOrdering(Ordering order);
+	
+	public List<Ordering> getAllOrdering();
 }
--- src/serviceImpl/OrderingServiceImpl.java
+++ src/serviceImpl/OrderingServiceImpl.java
@@ -1,6 +1,7 @@
 package serviceImpl;
 
 import java.util.Date;
+import java.util.List;
 
 import ch.ivyteam.ivy.environment.Ivy;
 import online.food.ordering.Ordering;
@@ -15,5 +16,9 @@ public class OrderingServiceImpl implements OrderingService {
 		order.setCreatedDate(new Date());
 		return Ivy.persistence().get(PERSISTENCE_UNIT_NAME).persist(order).getId();
 	}
-
+	
+	@Override
+	public List<Ordering> getAllOrdering() {
+		return (List<Ordering>) Ivy.persistence().get(PERSISTENCE_UNIT_NAME).findAll(Ordering.class);
+	}
 }
